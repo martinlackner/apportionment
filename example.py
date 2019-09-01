37584b976850afc1e23f63e353e49b9ab12361eb
Martin Lackner <unexpected@sent.at>
2019-09-01 17:00:23 +0200

    compatible with python3

--- example.py
+++ example.py
@@ -1,16 +1,16 @@
+from __future__ import print_function
 import apportionment
+
 
 distribution = [77, 22, 21, 10, 6]
 seats = 10
 
-print "distribution", "."*(25 - len("distribution")), distribution
-print
+print("distribution", "."*(25 - len("distribution")), distribution, "\n")
 
-print seats, "seats"
-print
+print(seats, "seats", "\n")
 
-print "apportionament results:"
+print("apportionment results:")
 for method in ["quota", "largest_remainder", "dhondt",
                "saintelague", "huntington", "adams"]:
     result = apportionment.method(method, distribution, seats, verbose=False)
-    print method, "."*(25 - len(method)), result
+    print(method, "."*(25 - len(method)), result)

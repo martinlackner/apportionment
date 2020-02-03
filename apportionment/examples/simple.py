92ac4244a6561fb781a6caeb7aaefc0209401322
Martin Lackner <unexpected@sent.at>
2020-02-03 20:22:25 +0100

    new example, all methods different

--- apportionment/examples/simple.py
+++ apportionment/examples/simple.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 import apportionment.methods as app
 
-votes = [77, 22, 21, 10, 6]
-seats = 10
+votes = [1, 3, 6, 7, 78]
+seats = 20
 
 print("votes", "."*(25 - len("votes")), votes, "\n")
 
@@ -10,6 +10,6 @@ print(seats, "seats", "\n")
 
 print("apportionment results:")
 for method in ["quota", "largest_remainder", "dhondt",
-               "saintelague", "huntington", "adams"]:
+               "saintelague", "huntington", "adams", "dean"]:
     result = app.compute(method, votes, seats, verbose=False)
     print(method, "."*(25 - len(method)), result)
